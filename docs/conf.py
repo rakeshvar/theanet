84ff36ffdacd8bcbd5e0e087a3108cdf5b31f35f
rakeshvar <rakeshvar@gmail.com>
2015-05-25 11:44:24 -0700

    Retry docs from scratch with mocks

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,21 @@ import shlex
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['numpy', 'numpy.distutils', 'scipy', 
+                'theano', 'theano.tensor', 'theano.tensor.signal',
+                'theano.tensor.signal.conv', 'theano.tensor.nnet',
+                'theano.tensor.nnet.conv',
+] 
+#['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
